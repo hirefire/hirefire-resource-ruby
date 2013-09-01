2322068cad9428d9537583b652a071bc0e0cbf5e
Stewart Knapman <info@stewartknapman.com>
2013-09-01 19:20:54 +1200

    use correct call to get count

--- lib/hirefire/macro/qc.rb
+++ lib/hirefire/macro/qc.rb
@@ -16,7 +16,7 @@ module HireFire
       #
       # @return [Integer] the number of jobs in the queue(s).
       def queue(queue = "default")
-        ::QC::Queue.new(:name => queue.to_s).count
+        ::QC::Worker.new(:q_name => queue.to_s).queue.count
       end
     end
   end

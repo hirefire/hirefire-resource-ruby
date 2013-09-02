895be2dfcb59e017e352216f84013eccaf23e204
Michael van Rooijen <meskyanichi@gmail.com>
2013-09-02 07:48:52 +0200

    Allow the developer to specify which mapper to use, rather than auto-detecting mappers. This can conflict when both mappers are defined.

--- lib/hirefire/macro/delayed_job.rb
+++ lib/hirefire/macro/delayed_job.rb
@@ -6,35 +6,48 @@ module HireFire
       module Job
         extend self
 
-        # Determines whether `ActiveRecord (3)` or `Mongoid` is being used.
-        # Once determined, it will build the appropriate query criteria in order
-        # to count the amount of jobs in a given queue and return the result.
+        # Returns the job quantity for the provided queue(s).
         #
         # @example Delayed::Job Macro Usage
-        #   HireFire::Macro::Delayed::Job.queue # all queues
-        #   HireFire::Macro::Delayed::Job.queue("email") # only email queue
-        #   HireFire::Macro::Delayed::Job.queue("audio", "video") # audio and video queues
+        #
+        #   # all queues using ActiveRecord mapper.
+        #   HireFire::Macro::Delayed::Job.queue(:mapper => :active_record)
+        #
+        #   # only "email" queue with Mongoid mapper.
+        #   HireFire::Macro::Delayed::Job.queue("email", :mapper => :mongoid)
+        #
+        #   # "audio" and "video" queues with ActiveRecord mapper.
+        #   HireFire::Macro::Delayed::Job.queue("audio", "video", :mapper => :active_record)
         #
         # @param [Array] queues provide one or more queue names, or none for "all".
+        #   Last argument can pass in a Hash containing :mapper => :active_record or :mapper => :mongoid
         #
         # @return [Integer] the number of jobs in the queue(s).
         def queue(*queues)
           queues.flatten!
 
-          if defined?(Mongoid)
-            c = ::Delayed::Job
-            c = c.where(:failed_at => nil)
-            c = c.where(:run_at.lte => Time.now.utc)
-            c = c.where(:queue.in => queues) unless queues.empty?
-            c.count
-          elsif defined?(ActiveRecord)
+          if queues.last.is_a?(Hash)
+            options = queues.pop
+          else
+            options = {}
+          end
+
+          case options[:mapper]
+          when :active_record
             c = ::Delayed::Job
             c = c.where(:failed_at => nil)
             c = c.where("run_at <= ?", Time.now.utc)
             c = c.where(:queue => queues) unless queues.empty?
             c.count
+          when :mongoid
+            c = ::Delayed::Job
+            c = c.where(:failed_at => nil)
+            c = c.where(:run_at.lte => Time.now.utc)
+            c = c.where(:queue.in => queues) unless queues.empty?
+            c.count
           else
-            raise "HireFire could not detect ActiveRecord or Mongoid for HireFire::Macro::Delayed::Job."
+            raise %{Must pass in :mapper => :active_record or :mapper => :mongoid\n} +
+              %{For example: HireFire::Macro::Delayed::Job.queue("worker", :mapper => :active_record)}
           end
         end
       end

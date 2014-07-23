a0690e534dadbba0e413ad1de5a755f13084e0de
Daniel Arnold <darnold8@gmail.com>
2014-07-23 17:22:52 -0400

    Insert middleware at index 0

--- lib/hirefire/railtie.rb
+++ lib/hirefire/railtie.rb
@@ -3,7 +3,7 @@
 module HireFire
   class Railtie < ::Rails::Railtie
     initializer "hirefire.add_middleware" do |app|
-      app.config.middleware.use "HireFire::Middleware"
+      app.config.middleware.insert 0, "HireFire::Middleware"
     end
   end
 end

766ed377fb358211cd9d8395318c57957735c236
Jan Klimo <jan.klimo@gmail.com>
2018-08-28 14:32:14 +0700

    Faster Sidekiq counts :rocket:

--- lib/hirefire/macro/sidekiq.rb
+++ lib/hirefire/macro/sidekiq.rb
@@ -28,9 +28,44 @@ module HireFire
           options = {}
         end
 
-        queues = queues.map(&:to_s)
-        queues = ::Sidekiq::Stats.new.queues.map { |name, _| name } if queues.empty?
+        queues.map!(&:to_s)
+        all_queues = ::Sidekiq::Queue.all.map(&:name)
+        queues = all_queues if queues.empty?
 
+        if fast_lookup_capable?(queues, all_queues)
+          fast_lookup(options)
+        else
+          dynamic_lookup(queues, options)
+        end
+      end
+
+      private
+
+      def fast_lookup_capable?(queues, all_queues)
+        # When no queue names are provided (or all of them are), we know we
+        # can peform much faster counts using Sidekiq::Stats and Redis
+        queues.sort == all_queues.sort
+      end
+
+      def fast_lookup(options)
+        stats = ::Sidekiq::Stats.new
+
+        in_queues = stats.enqueued
+
+        if !options[:skip_scheduled]
+          in_schedule = ::Sidekiq.redis { |c| c.zcount('schedule', '-inf', Time.now.to_f) }
+        end
+
+        if !options[:skip_retries]
+          in_retry = ::Sidekiq.redis { |c| c.zcount('retry', '-inf', Time.now.to_f) }
+        end
+
+        in_progress = stats.workers_size
+
+        [in_queues, in_schedule, in_retry, in_progress].compact.inject(&:+)
+      end
+
+      def dynamic_lookup(queues, options)
         in_queues = queues.inject(0) do |memo, name|
           memo += ::Sidekiq::Queue.new(name).size
           memo
@@ -38,25 +73,34 @@ module HireFire
 
         if !options[:skip_scheduled]
           max = options[:max_scheduled]
+
+          # For potentially long-running loops, compare all jobs against
+          # time when the set snapshot was taken to avoid incorrect counts.
+          now = Time.now
+
           in_schedule = ::Sidekiq::ScheduledSet.new.inject(0) do |memo, job|
-            memo += 1 if queues.include?(job["queue"]) && job.at <= Time.now
+            memo += 1 if queues.include?(job["queue"]) && job.at <= now
             break memo if max && memo >= max
             memo
           end
         end
 
         if !options[:skip_retries]
+          now = Time.now
+
           in_retry = ::Sidekiq::RetrySet.new.inject(0) do |memo, job|
-            memo += 1 if queues.include?(job["queue"]) && job.at <= Time.now
+            memo += 1 if queues.include?(job["queue"]) && job.at <= now
             memo
           end
         end
 
-        i = ::Sidekiq::VERSION >= "3.0.0" ? 2 : 1
-        in_progress = ::Sidekiq::Workers.new.inject(0) do |memo, job|
-          memo += 1 if queues.include?(job[i]["queue"]) && job[i]["run_at"] <= Time.now.to_i
-          memo
-        end
+        now = Time.now.to_i
+
+        # Objects yielded to Workers#each:
+        # https://github.com/mperham/sidekiq/blob/305ab8eedc362325da2e218b2a0e20e510668a42/lib/sidekiq/api.rb#L912
+        in_progress = ::Sidekiq::Workers.new.select do |key, tid, job|
+          queues.include?(job['queue']) && job['run_at'] <= now
+        end.size
 
         [in_queues, in_schedule, in_retry, in_progress].compact.inject(&:+)
       end

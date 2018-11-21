ae5c692c325513235ca0ec0ba753449bf28acdb0
Michael van Rooijen <michael@vanrooijen.io>
2018-11-21 13:13:22 +0100

    Cleaned up middleware and made it threadsafe.

--- lib/hirefire/middleware.rb
+++ lib/hirefire/middleware.rb
@@ -3,104 +3,108 @@
 module HireFire
   class Middleware
 
-    INFO_HEADERS = {
-      "Content-Type" => "application/json",
-      "Cache-Control" => "must-revalidate, private, max-age=0"
-    }
-
-    TEST_HEADERS = {
-      "Content-Type" => "text/html"
-    }
-
-    # Initialize the HireFire::Middleware and store the `app` in `@app`
-    # and `ENV["HIREFIRE_TOKEN"]` in `@token` for convenience.
+    # Initializes HireFire::Middleware.
     #
-    # @param [ActionDispatch::Routing::RouteSet] app.
+    # @param [Proc] app call with `env` to continue down the middleware stack.
     #
     def initialize(app)
-      @app   = app
-      @token = ENV["HIREFIRE_TOKEN"]
-      if defined?(Rails) && Rails.application.config.relative_url_root
-        @path_prefix = Regexp.new("^" + Regexp.escape(Rails.application.config.relative_url_root))
-      end
+      @app         = app
+      @token       = ENV["HIREFIRE_TOKEN"]
+      @path_prefix = get_path_prefix
     end
 
-    # Will respond to the request here if either the `test` or the `info` url was requested.
-    # Otherwise, fall through to the rest of the middleware below HireFire::Middleware.
+    # Intercepts and handles the /hirefire/test, /hirefire/development/info,
+    # and /hirefire/HIREFIRE_TOKEN/info paths. If none of these paths match,
+    # then then request will continue down the middleware stack.
+    #
+    # When HireFire::Resource.log_queue_metrics is enabled, and the HTTP_X_REQUEST_START
+    # header has been injected at the Heroku Router layer, queue time information will be
+    # logged to STDOUT. This data can be used by the HireFire Logdrain with the
+    # Web.Logplex.QueueTime autoscaling strategy.
+    #
+    # Important: Don't set/update instance variables within this- or any underlying methods.
+    # Doing so may result in race conditions when using threaded application servers.
     #
     # @param [Hash] env containing request information.
     #
     def call(env)
-      @env = env
-
       handle_queue(env["HTTP_X_REQUEST_START"])
 
-      if test?
-        [ 200, TEST_HEADERS, self ]
-      elsif info?
-        [ 200, INFO_HEADERS, self ]
+      if test_path?(env["PATH_INFO"])
+        build_test_response
+      elsif info_path?(env["PATH_INFO"])
+        build_info_response
       else
         @app.call(env)
       end
     end
 
-    # Returns text/html when the `test` url is requested.
-    # This is purely to see whether the URL works through the HireFire command-line utility.
-    #
-    # Returns a JSON String when the `info` url is requested.
-    # This url will be requested every minute by HireFire in order to fetch dyno data.
-    #
-    # @return [text/html, application/json] based on whether the `test` or `info` url was requested.
-    #
-    def each(&block)
-      if test?
-        block.call "HireFire Middleware Found!"
-      elsif info?
-        block.call(dynos)
-      end
-    end
-
     private
 
-    # Generates a JSON string based on the dyno data.
+    # Determines whether or not the test path has been requested.
     #
-    # @return [String] in JSON format.
+    # @param [String] path_info the requested path.
+    # @return [Boolean] true if the requested path matches the test path.
     #
-    def dynos
-      dyno_data = HireFire::Resource.dynos.inject(String.new) do |json, dyno|
-        json << %(,{"name":"#{dyno[:name]}","quantity":#{dyno[:quantity].call || "null"}})
-        json
-      end
-
-      "[#{dyno_data.sub(",","")}]"
+    def test_path?(path_info)
+      get_path(path_info) == "/hirefire/test"
     end
 
-    # Rack PATH_INFO with any RAILS_RELATIVE_URL_ROOT stripped off
+    # Determines whether or not the info path has been requested.
     #
-    # @return [String]
+    # @param [String] path_info the requested path.
+    # @return [Boolean] true if the requested path matches the info path.
     #
-    def path
+    def info_path?(path_info)
+      get_path(path_info) == "/hirefire/#{@token || "development"}/info"
+    end
+
+    # The provided path with @path_prefix stripped off.
+    #
+    # @param [String] path_info the requested path.
+    # @return [String] the path without the @path_prefix.
+    #
+    def get_path(path_info)
       if @path_prefix
-        @env["PATH_INFO"].gsub(@path_prefix, "")
+        path_info.gsub(@path_prefix, "")
       else
-        @env["PATH_INFO"]
+        path_info
       end
     end
 
-    # Returns true if the PATH_INFO matches the test url.
+    # Builds the response for the test path.
     #
-    # @return [Boolean] true if the requested url matches the test url.
+    # @return [String] in text/html format.
     #
-    def test?
-      path == "/hirefire/test"
+    def build_test_response
+      status  = 200
+      headers = {"Content-Type" => "text/html"}
+      body    = "HireFire Middleware Found!"
+
+      [status, headers, [body]]
     end
 
-    # Returns true if the PATH_INFO matches the info url.
+    # Builds the response for the info path containing the configured
+    # queues and their sizes based on the HireFire::Resource configuration.
     #
-    # @return [Boolean] true if the requested url matches the info url.
+    # @return [String] in application/json format.
     #
-    def info?
-      path == "/hirefire/#{@token || "development"}/info"
+    def build_info_response
+      entry_builder = lambda do |config|
+        %({"name":"#{config[:name]}","quantity":#{config[:quantity].call || "null"}})
+      end
+
+      entries = HireFire::Resource.dynos.inject([]) do |buffer, config|
+        buffer << entry_builder.call(config)
+      end
+
+      status                   = 200
+      headers                  = Hash.new
+      headers["Content-Type"]  = "application/json"
+      headers["Cache-Control"] = "must-revalidate, private, max-age=0"
+      body                     = "[" + entries.join(",") + "]"
+
+      [status, headers, [body]]
     end
 
     # Writes the Heroku Router queue time to STDOUT if a String was provided.
@@ -128,6 +132,15 @@ module HireFire
     def get_queue(value)
       ms = (Time.now.to_f * 1000).to_i - value.to_i
       ms < 0 ? 0 : ms
+    end
+
+    # Configures the @path_prefix in order to handle apps
+    # mounted under RAILS_RELATIVE_URL_ROOT.
+    #
+    def get_path_prefix
+      if defined?(Rails) && Rails.application.config.relative_url_root
+        Regexp.new("^" + Regexp.escape(Rails.application.config.relative_url_root))
+      end
     end
   end
 end

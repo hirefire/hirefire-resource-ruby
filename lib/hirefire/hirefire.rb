2fda3729fff9b4e58b609f7ff99dab9c0e98b1a5
Michael van Rooijen <michael@vanrooijen.io>
2023-11-11 12:41:15 +0100

    WIP

--- lib/hirefire/hirefire.rb
+++ lib/hirefire/hirefire.rb
@@ -2,19 +2,16 @@
 
 require "logger"
 
-# The `HireFire` module is the main entry point for integrating the
-# `hirefire-resource` gem into your application. It provides a
-# configuration interface to define how HireFire should collect,
-# serve, and dispatch metrics. These metrics are required for the
-# Heroku autoscaling decisions made by your dyno managers on
-# HireFire. Through this module, you can configure which metrics to
-# collect for web and worker dynos, and how these metrics should be
-# gathered. You can also specify a custom logger.
+# The `HireFire` module is the main entry point for integrating the `hirefire-resource` gem into
+# your application. It provides a configuration interface to define how HireFire should collect,
+# serve, and dispatch metrics. These metrics are required for the Heroku autoscaling decisions made
+# by your dyno managers on HireFire. Through this module, you can configure which metrics to collect
+# for web and worker dynos, and how these metrics should be gathered. You can also specify a custom
+# logger.
 #
-# This setup is usually done in an initializer within a Rails
-# application (e.g., config/initializers/hirefire.rb). For other
-# Ruby applications, the configuration should be placed in a part of
-# your codebase that is executed during application boot.
+# This setup is usually done in an initializer within a Rails application (e.g.,
+# config/initializers/hirefire.rb). For other Ruby applications, the configuration should be placed
+# in a part of your codebase that is executed during application boot.
 #
 # @example Configuring HireFire to collect metrics for web (i.e. Puma) and worker (i.e. Sidekiq)
 #   HireFire.configure do |config|

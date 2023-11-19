52e5a94abe36d2f44ed1a9248851922462bdfd23
Michael van Rooijen <michael@vanrooijen.io>
2023-11-19 10:48:55 +0100

    WIP

--- test/hirefire/test_web.rb
+++ test/hirefire/test_web.rb
@@ -3,15 +3,21 @@
 require "test_helper"
 
 class HireFire::WebTest < Minitest::Test
+  def web
+    @web ||= HireFire::Web.new
+  end
+
+  def log
+    @log ||= StringIO.new
+  end
+
   def setup
-    @log = StringIO.new
-    HireFire.configuration.logger = Logger.new(@log)
     ENV["HIREFIRE_TOKEN"] = "8ab101e2-51da-49bc-beba-111dec49a287"
     WebMock.reset_executed_requests!
+    HireFire.configuration.logger = Logger.new(log)
   end
 
   def test_starts_and_stops_correctly
-    web = HireFire::Web.new
     refute web.dispatcher_running?
     web.start_dispatcher
     assert web.dispatcher_running?
@@ -19,12 +25,11 @@ class HireFire::WebTest < Minitest::Test
     web.stop_dispatcher
     assert_empty web.send :flush_buffer
     refute web.dispatcher_running?
-    assert_includes @log.string, "[HireFire] Starting web metrics dispatcher."
-    assert_includes @log.string, "[HireFire] Web metrics dispatcher stopped."
+    assert_includes log.string, "[HireFire] Starting web metrics dispatcher."
+    assert_includes log.string, "[HireFire] Web metrics dispatcher stopped."
   end
 
   def test_buffer_addition
-    web = HireFire::Web.new
     web.add_to_buffer(1)
     buffer_contents = web.send :flush_buffer
     refute_empty buffer_contents
@@ -32,7 +37,6 @@ class HireFire::WebTest < Minitest::Test
   end
 
   def test_buffer_flushing
-    web = HireFire::Web.new
     web.add_to_buffer(2)
     web.send :flush_buffer
     buffer_contents_after_flush = web.send :flush_buffer
@@ -40,65 +44,58 @@ class HireFire::WebTest < Minitest::Test
   end
 
   def test_successful_dispatch_post
-    web = HireFire::Web.new
     request = stub_request(:post, "https://logdrain.hirefire.io/")
       .with(headers: {"HireFire-Resource" => "Ruby-#{HireFire::VERSION}"})
       .to_return(status: 200)
     web.add_to_buffer(5)
     web.send :dispatch_buffer
-    assert @log.string.empty?
+    assert log.string.empty?
     assert_requested request
   end
 
   def test_dispatch_post_with_unexpected_response_code
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_return(status: 404)
     web.add_to_buffer(5)
     web.send :dispatch_buffer
-    assert_includes @log.string,
+    assert_includes log.string,
       "[HireFire] Error while dispatching web metrics: " \
       "An unexpected error occurred (Unexpected response code 404.).\n"
   end
 
   def test_dispatch_post_with_generic_exception
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_raise(StandardError.new("Some generic error"))
     web.add_to_buffer(8)
     web.send :dispatch_buffer
-    assert_includes @log.string,
+    assert_includes log.string,
       "[HireFire] Error while dispatching web metrics: An unexpected error occurred (Some generic error)."
   end
 
   def test_dispatch_post_with_server_error
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_return(status: 500)
     web.add_to_buffer(4)
     web.send :dispatch_buffer
-    assert_includes @log.string,
+    assert_includes log.string,
       "[HireFire] Error while dispatching web metrics: " \
       "An unexpected error occurred (Server responded with 500 status.)."
   end
 
   def test_dispatch_post_with_timeout
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_timeout
     web.add_to_buffer(5)
     web.send :dispatch_buffer
-    assert_includes @log.string,
+    assert_includes log.string,
       "[HireFire] Error while dispatching web metrics: Request timed out."
   end
 
   def test_dispatch_post_with_network_error
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_raise(SocketError.new("Failed"))
     web.add_to_buffer(6)
     web.send :dispatch_buffer
-    assert_includes @log.string,
+    assert_includes log.string,
       "[HireFire] Error while dispatching web metrics: Network error occurred (Failed)."
   end
 
   def test_buffer_repopulation_after_dispatch_failure
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_return(status: 500)
     web.add_to_buffer(7)
     web.send :dispatch_buffer
@@ -107,7 +104,6 @@ class HireFire::WebTest < Minitest::Test
   end
 
   def test_buffer_ttl_discards_old_entries
-    web = HireFire::Web.new
     stub_request(:post, "https://logdrain.hirefire.io/").to_return(status: 500)
     web.add_to_buffer(7)
     past_timestamp = Time.now.to_i - HireFire::Web::BUFFER_TTL - 10

79ae92b3d71b223bed1145664727f97bd3694479
Michael van Rooijen <michael@vanrooijen.io>
2023-12-20 15:14:59 +0100

    WIP

--- lib/hirefire/macro/legacy/queue_classic.rb
+++ lib/hirefire/macro/legacy/queue_classic.rb
@@ -3,17 +3,21 @@
 module HireFire
   module Macro
     module Legacy
+      # Provides backward compatibility with the legacy QC Macro.
+      # For new implementations, refer to {HireFire::Macro::QC}.
       module QC
-        # Queries the PostgreSQL database through QueueClassic in order to
-        # count the amount of jobs in the specified queue.
+        # Retrieves the total number of jobs in the specified queue using QueueClassic.
         #
-        # @example QueueClassic Macro Usage
-        #   HireFire::Macro::QC.queue # counts the `default` queue.
-        #   HireFire::Macro::QC.queue("email") # counts the `email` queue.
+        # This method queries the PostgreSQL database through QueueClassic. It's capable
+        # of counting jobs in a specific queue, defaulting to the 'default' queue if none is specified.
         #
-        # @param [String, Symbol, nil] queue the queue name to count. (default: `default`)
-        # @return [Integer] the number of jobs in the queue(s).
-        #
+        # @param queue [String, Symbol] The name of the queue to count.
+        #   Defaults to 'default' if no queue name is provided.
+        # @return [Integer] Total number of jobs in the specified queue.
+        # @example Counting jobs in the 'default' queue
+        #   HireFire::Macro::QC.queue
+        # @example Counting jobs in the 'email' queue
+        #   HireFire::Macro::QC.queue("email")
         def queue(queue = "default")
           ::QC::Queue.new(queue).count
         end
